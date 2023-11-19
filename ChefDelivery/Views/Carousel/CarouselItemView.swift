bbf923b9405756ca2399480624119702303f792b
igormaxlima <igormaxnunes@gmail.com>
2023-11-19 16:04:55 -0300

    correcting error merging branches

---
+++ ChefDelivery/Views/Carousel/CarouselItemView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselItemView.swift
+//  ChefDelivery
+//
+//  Created by Igor Max de Lima Nunes on 19/11/23.
+//
+
+import SwiftUI
+
+struct CarouselItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselItemView()
+}
